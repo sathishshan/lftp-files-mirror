c11219838244454b94ffb93d059b8a1d4f67e3f2
Sathish Kumar <23581693+Sathishshan@users.noreply.github.com>
2019-06-30 11:06:17 +0530

    Create tmp_use.sh

---
+++ tmp_use.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+#usage
+function usage {
+	echo "USAGE: For MAN page use -m [help command]"
+	echo "$0 -u Username -p 'Password' -h Host -d Domain.com -f path_to_remotefile.txt"
+}
+
+while getopts ":u:p:h:d:f:m" opt; do
+	case "$opt" in
+		u)
+			FTPNAME=$OPTARG
+			;;
+		p)
+			FTPPASS=$OPTARG
+			;;
+		h) 
+			HOST=$OPTARG
+			;;
+		d) 
+			DOMAIN=$OPTARG
+			;;
+		f)
+			REMOTE_FILE=$OPTARG
+			;;
+		m | *)
+		 	usage
+		 	exit
+		 	;;
+		?)
+     echo "I don't know what $OPTARG is !!!"
+     Usage
+     exit 1
+			;;
+	esac
+done
+
+#Passing Arguments
+
+# Creating lftp_runner
+echo "
+set net:timeout 10
+set ssl:verify-certificate false
+set ftp:ssl-allow no
+set ftp:passive-mode true
+set ftp:list-options -a
+
+open $HOST
+user $FTPNAME \"$FTPPASS\"" > lftp_runner.txt
+
+cat "$REMOTE_FILE" 2> /dev/null | while read line
+do
+echo "mget -d ${line}" >> lftp_runner.txt
+done
+# Executing lftp runner
+
+lftp -f ./lftp_runner.txt
+echo "Mirroring Completed, Successfully!"
