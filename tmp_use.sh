6fde632a39d2db8127c9dffa5519ab678aaa15f5
sathish shan <noreply@sathish.com>
2019-07-01 04:54:46 +0530

    removed domain flag from getopts

--- tmp_use.sh
+++ tmp_use.sh
@@ -3,10 +3,10 @@
 #usage
 function usage {
 	echo "USAGE: For MAN page use -m [help command]"
-	echo "$0 -u Username -p 'Password' -h Host -d Domain.com -f path_to_remotefile.txt"
+	echo "$0 -u Username -p 'Password' -h Host -f path_to_remotefile.txt"
 }
 
-while getopts ":u:p:h:d:f:m" opt; do
+while getopts ":u:p:h:f:m" opt; do
 	case "$opt" in
 		u)
 			FTPNAME=$OPTARG
@@ -17,9 +17,6 @@ while getopts ":u:p:h:d:f:m" opt; do
 		h) 
 			HOST=$OPTARG
 			;;
-		d) 
-			DOMAIN=$OPTARG
-			;;
 		f)
 			REMOTE_FILE=$OPTARG
 			;;
@@ -28,9 +25,9 @@ while getopts ":u:p:h:d:f:m" opt; do
 		 	exit
 		 	;;
 		?)
-     echo "I don't know what $OPTARG is !!!"
-     Usage
-     exit 1
+			echo "I don't know what $OPTARG is !!!"
+			Usage
+			exit 1
 			;;
 	esac
 done
